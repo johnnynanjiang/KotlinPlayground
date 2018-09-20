5d3f874449c2f8f61f9eb14e847256722b158119
Johnny Jiang <Johnny.Jiang@suncorp.com.au>
2018-09-20 10:37:36 +1000

    - add test for sealed class

--- app/src/test/java/io/github/johnnynanjiang/android/kotlinplayground/PlaygroundUniTest.kt
+++ app/src/test/java/io/github/johnnynanjiang/android/kotlinplayground/PlaygroundUniTest.kt
@@ -8,28 +8,62 @@ import org.junit.Assert.*
  */
 
 class PlaygroundUniTest {
-    data class ImmutableDataClass(val name: String, val age: Int)
-    data class MutableDataClass(var name: String, var age: Int)
-
     @Test
     fun immutableDataClassTest() {
-        var objOfImmutableDataClass = ImmutableDataClass("Max", 9)
+        data class TestDataClass(val name: String, val age: Int)
 
-        assertEquals("Max", objOfImmutableDataClass.name)
-        assertEquals(9, objOfImmutableDataClass.age)
+        var obj = TestDataClass("Max", 9)
+
+        assertEquals("Max", obj.name)
+        assertEquals(9, obj.age)
     }
 
     @Test
     fun mutableDataClassTest() {
-        val objOfMutableDataClass = MutableDataClass("Max", 9)
+        data class TestDataClass(var name: String, var age: Int)
 
-        assertEquals("Max", objOfMutableDataClass.name)
-        assertEquals(9, objOfMutableDataClass.age)
+        val obj = TestDataClass("Max", 9)
 
-        objOfMutableDataClass.name = "Mia"
-        objOfMutableDataClass.age = 3
+        assertEquals("Max", obj.name)
+        assertEquals(9, obj.age)
 
-        assertEquals("Mia", objOfMutableDataClass.name)
-        assertEquals(3, objOfMutableDataClass.age)
+        obj.name = "Mia"
+        obj.age = 3
+
+        assertEquals("Mia", obj.name)
+        assertEquals(3, obj.age)
+    }
+
+    @Test
+    fun deaultValuesInConstructor() {
+        class TestDataClass(val param1 = "param1", val param2 = "param2")
+
+        val obj(param2 = "param2_new")
+
+        assertEquals("param1", obj.param1)
+        assertEquals("param2_new", obj.param2)
+    }
+
+    @Test
+    fun sealedClass() {
+        sealed class TestSealedClass()
+
+        class SubClassA(val property1 = "property1") : TestSealedClass
+        data class SubClassB(val property1 = "property1") : TestSealedClass
+        object SubClassC(val property1 = "property1") : TestSealedClass
+
+        val objA = SubClassA()
+        val objB = SubClassB()
+        val objC = SubClassC()
+
+        for(obj in [objA, objB, objC]) {
+            val result = when(param) {
+                is SubClassA -> "SubClassA"
+                is SubClassB -> "SubClassB"
+                SubClassC -> "SubClassC"
+            }
+
+            println(result)
+        }
     }
 }
